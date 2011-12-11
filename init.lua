f242b146179a98f375540764b83730dff1bd3641
teddydestodes <derkomtur@schattengang.net>
2011-12-11 18:25:15 +0100

    made it compatible with latest lua api

--- init.lua
+++ init.lua
@@ -6,46 +6,50 @@ end
 -- items
 
 minetest.register_craft({
-	output = 'CraftItem "lever_wood" 2',
+	output = 'CraftItem "mechanics:lever_wood" 2',
 	recipe = {
 		{'CraftItem "Stick"'},
 	}
 })
 
-minetest.register_craftitem("lever_wood", {
+minetest.register_craftitem("mechanics:lever_wood", {
 	image = "lever_wood.png",
 	stack_max = 99,
 	on_place_on_ground = craftitem_place_item,
 })
 
 minetest.register_craft({
-	output = 'CraftItem "axle_wood" 1',
+	output = 'NodeItem "mechanics:axle_wood" 1',
 	recipe = {
 		{'CraftItem "Stick"', 'CraftItem "Stick"'},
 	}
 })
 
-minetest.register_craftitem("axle_wood", {  -- axle should be a NodeItem
-	image = "axle_wood.png",
-	stack_max = 99,
-	on_place_on_ground = craftitem_place_item,
+minetest.register_node("mechanics:axle_wood", {
+	drawtype = "axlelike",
+	tile_images = {"axle_wood_node.png"},
+	inventory_image = "axle_wood.png",
+	is_ground_content = true,
+	material = minetest.digprop_woodlike(0.75),
+	furnace_burntime = 15,
 })
 
-minetest.register_craftitem("screw_wood", {
+
+minetest.register_craftitem("mechanics:screw_wood", {
 	image = "screw_wood.png",
 	stack_max = 99,
 	on_place_on_ground = craftitem_place_item,
 })
 
 minetest.register_craft({
-	output = 'CraftItem "screw_wood" 1',
+	output = 'CraftItem "mechanics:screw_wood" 1',
 	recipe = {
-		{'NodeItem "wood"', 'CraftItem "axle_wood"', 'NodeItem "wood"'},
+		{'NodeItem "wood"', 'CraftItem "mechanics:axle_wood"', 'NodeItem "wood"'},
 	}
 })
 
 minetest.register_craft({
-	output = 'CraftItem "gear_wood" 1',
+	output = 'CraftItem "mechanics:gear_wood" 1',
 	recipe = {
 		{'', 'CraftItem "Stick"', ''},
 		{'CraftItem "Stick"', 'NodeItem "wood"', 'CraftItem "Stick"'},
@@ -53,14 +57,14 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craftitem("gear_wood", {
+minetest.register_craftitem("mechanics:gear_wood", {
 	image = "gear_wood.png",
 	stack_max = 99,
 	on_place_on_ground = craftitem_place_item,
 })
 
 minetest.register_craft({
-	output = 'CraftItem "gear_big_wood" 1',
+	output = 'CraftItem "mechanics:gear_big_wood" 1',
 	recipe = {
 		{'CraftItem "Stick"', 'CraftItem "Stick"', 'CraftItem "Stick"'},
 		{'CraftItem "Stick"', 'NodeItem "wood"', 'CraftItem "Stick"'},
@@ -68,7 +72,7 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craftitem("gear_big_wood", {
+minetest.register_craftitem("mechanics:gear_big_wood", {
 	image = "gear_big_wood.png",
 	stack_max = 99,
 	on_place_on_ground = craftitem_place_item,
@@ -87,31 +91,32 @@ function digprop_woodlike(toughness)
 end
 
 minetest.register_craft({
-	output = 'NodeItem "gearbox_wood" 1',
+	output = 'NodeItem "mechanics:gearbox_wood" 1',
 	recipe = {
 		{'NodeItem "wood"', 'NodeItem "wood"', 'NodeItem "wood"'},
-		{'NodeItem "wood"', 'CraftItem "gear_wood"', 'NodeItem "wood"'},
+		{'NodeItem "wood"', 'CraftItem "mechanics:gear_wood"', 'NodeItem "wood"'},
 		{'NodeItem "wood"', 'NodeItem "wood"', 'NodeItem "wood"'},
 	}
 })
 
-minetest.register_node("gearbox_wood", {
+minetest.register_node("mechanics:gearbox_wood", {
+	drawtype = "normal",
 	tile_images = {"gearbox_hole_wood.png"},
-	inventory_image = inventorycube("gearbox_hole_wood.png"),
+	inventory_image = minetest.inventorycube("gearbox_hole_wood.png"),
 	is_ground_content = true,
-	material = digprop_woodlike(0.75),
+	material = minetest.digprop_woodlike(0.75),
 	furnace_burntime = 30,
 })
 
 minetest.register_craft({
-	output = 'NodeItem "crank_wood" 1',
+	output = 'NodeItem "mechanics:crank_wood" 1',
 	recipe = {
-		{'CraftItem "lever_wood"', 'CraftItem "lever_wood"'},
-		{'CraftItem "lever_wood"', ''},
+		{'CraftItem "mechanics:lever_wood"', 'CraftItem "mechanics:lever_wood"'},
+		{'CraftItem "mechanics:lever_wood"', ''},
 	}
 })
 
-minetest.register_node("crank_wood", {
+minetest.register_node("mechanics:crank_wood", {
 	drawtype = "torchlike",
 	tile_images = {"crank_wood_floor.png", "crank_wood_ceiling.png", "crank_wood_wall.png"},
 	inventory_image = "crank_wood_wall.png",
@@ -123,6 +128,6 @@ minetest.register_node("crank_wood", {
 		wall_bottom = {-0.3, -0.5, -0.3, 0.1, 0, 0.1},
 		wall_side = {-0.5, -0.4, -0.1, 0, 0.1, 0.1},
 	},
-	material = digprop_constanttime(0.0),
+	material = minetest.digprop_constanttime(0.0),
 	furnace_burntime = 4,
 })
